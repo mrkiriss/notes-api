3e5e20065d7bd8362a60795d85da63687665f605
Kirill Voskrebentsev <kvoskrebentsev@mail.ru>
2026-01-07 12:26:41 +0300

    add deps

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,17 @@
 val kotlin_version: String by project
 val logback_version: String by project
+val exposed_version: String by project
+val flyway_version: String by project
+val postgres_version: String by project
+val ktlint_version: String by project
+val junit_version: String by project
 
 plugins {
-    kotlin("jvm") version "2.2.21"
-    id("io.ktor.plugin") version "3.3.2"
+    kotlin("jvm")
+    kotlin("plugin.serialization")
+    id("io.ktor.plugin")
+    id("io.gitlab.arturbosch.detekt")
+    id("org.jlleitschuh.gradle.ktlint")
 }
 
 group = "ru.miet.kvosk.notes"
@@ -13,17 +21,43 @@ application {
     mainClass = "io.ktor.server.netty.EngineMain"
 }
 
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(17)
+    }
+}
+
+ktlint {
+    version.set(ktlint_version)
+}
+
+tasks.withType<io.gitlab.arturbosch.detekt.Detekt>().configureEach {
+    jvmTarget = "17"
+}
+
 dependencies {
     implementation("io.ktor:ktor-server-core")
     implementation("io.ktor:ktor-server-content-negotiation")
+    implementation("io.ktor:ktor-serialization-kotlinx-json")
     implementation("io.ktor:ktor-server-host-common")
     implementation("io.ktor:ktor-server-status-pages")
     implementation("io.ktor:ktor-server-call-logging")
     implementation("io.ktor:ktor-server-openapi")
     implementation("io.ktor:ktor-server-swagger")
     implementation("io.ktor:ktor-server-netty")
+    implementation("org.flywaydb:flyway-core:$flyway_version")
+    implementation("org.postgresql:postgresql:$postgres_version")
+    implementation("org.jetbrains.exposed:exposed-core:$exposed_version")
+    implementation("org.jetbrains.exposed:exposed-dao:$exposed_version")
+    implementation("org.jetbrains.exposed:exposed-jdbc:$exposed_version")
+    implementation("org.jetbrains.exposed:exposed-java-time:$exposed_version")
     implementation("ch.qos.logback:logback-classic:$logback_version")
     implementation("io.ktor:ktor-server-config-yaml")
     testImplementation("io.ktor:ktor-server-test-host")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
+    testImplementation("org.junit.jupiter:junit-jupiter:$junit_version")
+}
+
+tasks.test {
+    useJUnitPlatform()
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,5 +1,24 @@
 rootProject.name = "notes-project"
 
+pluginManagement {
+    val kotlinVersion = providers.gradleProperty("kotlin_version").get()
+    val ktorVersion = providers.gradleProperty("ktor_version").get()
+    val detektVersion = providers.gradleProperty("detekt_version").get()
+    val ktlintVersion = providers.gradleProperty("ktlint_version").get()
+
+    plugins {
+        kotlin("jvm") version kotlinVersion
+        kotlin("plugin.serialization") version kotlinVersion
+        id("io.ktor.plugin") version ktorVersion
+        id("io.gitlab.arturbosch.detekt") version detektVersion
+        id("org.jlleitschuh.gradle.ktlint") version ktlintVersion
+    }
+    repositories {
+        gradlePluginPortal()
+        mavenCentral()
+    }
+}
+
 dependencyResolutionManagement {
     repositories {
         mavenCentral()
