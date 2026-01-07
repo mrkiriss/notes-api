6eb6c4afb9938199044d80219f19e2f3466ee4e8
Kirill Voskrebentsev <kvoskrebentsev@mail.ru>
2026-01-07 13:25:40 +0300

    fix test

--- src/test/kotlin/ru/miet/kvosk/notes/ApplicationTest.kt
+++ src/test/kotlin/ru/miet/kvosk/notes/ApplicationTest.kt
@@ -3,8 +3,8 @@ package ru.miet.kvosk.notes
 import io.ktor.client.request.get
 import io.ktor.http.HttpStatusCode
 import io.ktor.server.testing.testApplication
-import kotlin.test.Test
-import kotlin.test.assertEquals
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
 
 class ApplicationTest {
     @Test
